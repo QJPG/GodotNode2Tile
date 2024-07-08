cdbcd74d6ce302fce1fbe3d9002b843380a353ca
QJPG <53179496+QJPG@users.noreply.github.com>
2024-07-07 21:33:05 -0300

    Fixed resources path on import plugin

--- GodotNode2Tile/main_script.gd
+++ GodotNode2Tile/main_script.gd
@@ -5,18 +5,18 @@ extends EditorPlugin
 func _enter_tree():
 	add_custom_type("Brush",
 		"Brush",
-		preload("res://addons/GodotNode2Tile/src/cl_brush.gd"),
-		preload("res://addons/GodotNode2Tile/misc/icon_brush.png"))
+		preload("../GodotNode2Tile/src/cl_brush.gd"),
+		preload("../GodotNode2Tile/misc/icon_brush.png"))
 	
 	add_custom_type("BrushForm",
 		"BrushForm",
-		preload("res://addons/GodotNode2Tile/src/cl_form.gd"),
-		preload("res://addons/GodotNode2Tile/misc/icon_form.png"))
+		preload("../GodotNode2Tile/src/cl_form.gd"),
+		preload("../GodotNode2Tile/misc/icon_form.png"))
 	
 	add_custom_type("VertexAttachment",
 		"VertexAttachment",
-		preload("res://addons/GodotNode2Tile/src/cl_vertex_attachment.gd"),
-		preload("res://addons/GodotNode2Tile/misc/icon_vatt.png"))
+		preload("../GodotNode2Tile/src/cl_vertex_attachment.gd"),
+		preload("../GodotNode2Tile/misc/icon_vatt.png"))
 
 
 func _exit_tree():
